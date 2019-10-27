0dd24c2b9662f969ecd8443e8792e8f5008e16c7
Vivek Sekhar <vivek@viveksekhar.ca>
2019-10-27 13:30:18 -0400

    Hacking on capture.

--- testdata/capture/capture.js
+++ testdata/capture/capture.js
@@ -1,5 +1,6 @@
 const puppeteer = require('puppeteer-core');
-const commander = require('minimist');
+const path = require('path');
+const fse = require('fs-extra');
 var argv = require('minimist')(process.argv.slice(2));
 
 (async () => {
@@ -9,8 +10,25 @@ var argv = require('minimist')(process.argv.slice(2));
     });
     const page = await browser.newPage();
     await page.setViewport({ width: 1366, height: 768});
-    await page.goto('https://google.com');
-    await page.screenshot({path: 'google.png'});
+    page.on('requestfinished', async (request) => {
+      const url = new URL(response.url());
+
+    });
+    page.on('response', async (response) => {
+      // redirects
+      if (response.status() >= 300 && response.status() < 400) {
+        return;
+      }
+      const url = new URL(response.url());
+      let filePath = path.resolve(`./output${url.pathname}`);
+      if (path.extname(url.pathname).trim() === '') {
+        filePath = `${filePath}/index.html`;
+      }
+      await fse.outputFile(filePath, await response.buffer());
+    });
+    await page.goto('https://google.com', {
+      waitUntil: 'networkidle2'
+    });
   
     await browser.close();
   })();
