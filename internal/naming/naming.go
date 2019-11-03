c838d7526b436252afcad998776b6b2c083c5e6d
Vivek Sekhar <vivek@viveksekhar.ca>
2019-11-03 13:37:26 -0500

    Factor out replacements, set case last.
    Setting case last ensures case is set for replacements (e.g. "_PLUS_") as well.

--- internal/naming/naming.go
+++ internal/naming/naming.go
@@ -2,28 +2,29 @@ package naming
 
 import "strings"
 
+var replacements = []string{
+	"/", "_",
+	".", "_",
+	"+", "_PLUS_",
+	" ", "_",
+	"-", "_",
+	"(", "_",
+	")", "_",
+	";", "_",
+	":", "_",
+	"*", "_STAR_",
+	"\n", "_",
+	" ", "_",
+}
+var repl = strings.NewReplacer(replacements...)
+
 // ProtoFieldName returns a representation of s that is suitable
 // to be a field name for a protocol buffer.
 //
 // E.g. ProtoFieldName("Accept-Encoding") == "accept_encoding"
 func ProtoFieldName(s string) string {
-	s = strings.ToLower(s)
-	replacements := []string{
-		"/", "_",
-		".", "_",
-		"+", "_PLUS_",
-		" ", "_",
-		"-", "_",
-		"(", "_",
-		")", "_",
-		";", "_",
-		":", "_",
-		"*", "_STAR_",
-		"\n", "_",
-		" ", "_",
-	}
-	repl := strings.NewReplacer(replacements...)
 	s = repl.Replace(s)
+	s = strings.ToLower(s)
 	s = strings.Trim(s, "_")
 	return s
 }
@@ -33,23 +34,8 @@ func ProtoFieldName(s string) string {
 //
 // E.g. ProtoEnumName("utf-8") == "UTF_8"
 func ProtoEnumName(s string) string {
-	s = strings.ToUpper(s)
-	replacements := []string{
-		"/", "_",
-		".", "_",
-		"+", "_PLUS_",
-		" ", "_",
-		"-", "_",
-		"(", "_",
-		")", "_",
-		";", "_",
-		":", "_",
-		"*", "_STAR_",
-		"\n", "_",
-		" ", "_",
-	}
-	repl := strings.NewReplacer(replacements...)
 	s = repl.Replace(s)
+	s = strings.ToUpper(s)
 	s = strings.Trim(s, "_")
 	return s
 }
